64d39143927e4c6986dfb863bc360736d494d7ec
KingKong <brockmann.malek@gmail.com>
2025-01-06 14:41:18 +0100

    Taliwind und bessere triggerfunktion hinzugefügt

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"], // Alle HTML- und JS-Dateien im Projektordner
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
